4cd3951681fc0d8b41224d73bf7b1ac0b3c20e9c
TathastuAgarwala <tathastuagarwala0@gmail.com>
2026-01-23 01:20:16 +0530

    fix port for render

--- index.js
+++ index.js
@@ -83,6 +83,8 @@ app.get("/search", async (req, res) => {
 });
 
 // START SERVER
-app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
